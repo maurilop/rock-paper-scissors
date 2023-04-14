5d13dbcfc905dcd4fa1d449a3fe1f756012a0b04
Mauricio López <maurilop92@gmail.com>
2023-04-13 21:15:15 -0300

    Update playRound function to return result instead of logging it into the console

--- script.js
+++ script.js
@@ -11,38 +11,39 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-let computerChoice = getComputerChoice();
-
 function playRound(playerSelection, computerSelection) {
+  let roundResult;
   if (playerSelection === "rock" && computerSelection == "paper") {
-    console.log("You lose! Paper beats Rock");
+    roundResult = "You lose! Paper beats Rock";
   } else if (playerSelection === "rock" && computerSelection == "scissors") {
-    console.log("You win! Rock beats Scissors");
+    roundResult = "You win! Rock beats Scissors";
   } else if (playerSelection === "rock" && computerSelection == "rock") {
-    console.log("It's a tie! Both choose Rock");
+    roundResult = "It's a tie! Both choose Rock";
   } else if (playerSelection === "paper" && computerSelection == "scissors") {
-    console.log("You lose! Scissors beats Paper");
+    roundResult = "You lose! Scissors beats Paper";
   } else if (playerSelection === "paper" && computerSelection == "rock") {
-    console.log("You win! Paper beats Rock");
+    roundResult = "You win! Paper beats Rock";
   } else if (playerSelection === "paper" && computerSelection == "paper") {
-    console.log("It's a tie! Both choose Paper");
+    roundResult = "It's a tie! Both choose Paper";
   } else if (playerSelection === "scissors" && computerSelection == "rock") {
-    console.log("You lose! Rock beats Scissors");
+    roundResult = "You lose! Rock beats Scissors";
   } else if (playerSelection === "scissors" && computerSelection == "paper") {
-    console.log("You win! Scissors beats Paper");
+    roundResult = "You win! Scissors beats Paper";
   } else if (
     playerSelection === "scissors" &&
     computerSelection == "scissors"
   ) {
-    console.log("It's a tie! Both choose Scissors");
+    roundResult = "It's a tie! Both choose Scissors";
   } else {
-    console.log("Stop breaking the rules dude!");
+    roundResult = "Stop breaking the game dude!";
   }
+  return roundResult;
 }
 
+let computerChoice = getComputerChoice();
 let playerChoice = prompt("What's your move, bitch?").toLowerCase();
 
 console.log("Player choice: " + playerChoice);
 console.log("Computer choice: " + computerChoice);
 
-playRound(playerChoice, computerChoice);
+console.log(playRound(playerChoice, computerChoice));
